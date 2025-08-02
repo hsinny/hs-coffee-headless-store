ce466bd04fd53e7f3e777fa865d9d14d5a9144f1
hsinny <hsinnyliu@gmail.com>
2025-08-02 22:41:10 +0800

    feat: 增加結帳時為郵寄方式，須驗證必要的地址欄位
    - 另增加請求結帳時，驗證前端訂單資料以確保和 WP 後端資料一致

--- includes/class-wc-store-checkout.php
+++ includes/class-wc-store-checkout.php
@@ -12,6 +12,7 @@ class WC_Store_Checkout {
 	public function __construct() {
 		add_filter( 'woocommerce_default_address_fields', array( $this, 'headless_modify_default_address_fields' ), 10, 1 );
 		add_filter( 'rest_post_dispatch', array( $this, 'add_cvs_meta_data_to_draft_checkout_response' ), 10, 3 );
+		add_action( 'woocommerce_store_api_checkout_update_order_from_request', array( $this, 'headless_validate_order_fields' ), 10, 2 );
 	}
 
 	/**
@@ -75,4 +76,73 @@ class WC_Store_Checkout {
 
 		return $response;
 	}
+
+	/**
+	 * 結帳過程中驗證訂單欄位
+	 *
+	 * 確保前台提供資料與後台資料相符、確保郵寄時，有提供完整運送地址
+	 * 如果驗證失敗，拋出 RouteException。
+	 *
+	 * @param WC_Order        $order   The WooCommerce order object.
+	 * @param WP_REST_Request $request The REST API request object containing order data.
+	 *
+	 * @throws RouteException if any validation fails.
+	 */
+	function headless_validate_order_fields( $order, $request ) {
+		$payload  = $request->get_json_params();
+		$order_id = intval( $request->get_param( 'order_id' ) ); // 加上清理輸入資料 check 是否需要
+		$order    = wc_get_order( $order_id );
+		if ( ! $order ) {
+			throw new RouteException( 'headless_order_not_found', '找不到訂單', 404 );
+		}
+
+		// 先比對前端和後端值是否一致，驗證失敗返回 error 給前端中斷結帳
+		$frontend_total = $payload['total_amount'];
+		$order_total    = $order->get_total();
+		if ( $frontend_total !== $order_total ) {
+			throw new RouteException( 'headless_checkout_total_mismatch', '訂單金額不一致，請重新整理頁面再操作一次', 400 );
+		}
+
+		$frontend_shipping_method_id = $payload['shipping_method_id'];
+		$shipping_method_id          = get_selected_shipping_method_id( $order );
+		if ( $frontend_shipping_method_id !== $shipping_method_id ) {
+			throw new RouteException( 'headless_checkout_shipping_mismatch', '運送方式不一致，請重新整理頁面再操作一次', 400 );
+		}
+
+		if ( strpos( $shipping_method_id, 'Wooecpay_Logistic_CVS' ) !== false ) {
+			$frontend_receiver_store_id = $payload['receiver_store_id'];
+			$CVSStoreID                 = $order->get_meta( '_ecpay_logistic_cvs_store_id' );
+			if ( $frontend_receiver_store_id !== $CVSStoreID ) {
+				throw new RouteException( 'headless_checkout_cvs_store_mismatch', '取貨門市不一致，請重新整理頁面再操作一次', 400 );
+			}
+		}
+
+		// 郵寄寄送，須驗證的地址欄位
+		if ( strpos( $shipping_method_id, 'flat_rate' ) !== false ) {
+			$required_fields = array(
+				'billing_address_1'  => __( 'Billing Address 1', 'woocommerce' ),
+				'billing_city'       => __( 'Billing City', 'woocommerce' ),
+				'billing_postcode'   => __( 'Billing Postcode', 'woocommerce' ),
+				'billing_country'    => __( 'Billing Country', 'woocommerce' ),
+				'shipping_address_1' => __( 'Shipping Address 1', 'woocommerce' ),
+				'shipping_city'      => __( 'Shipping City', 'woocommerce' ),
+				'shipping_postcode'  => __( 'Shipping Postcode', 'woocommerce' ),
+				'shipping_country'   => __( 'Shipping Country', 'woocommerce' ),
+			);
+
+			// 檢查必填欄位是否為空
+			foreach ( $required_fields as $field_key => $field_label ) {
+				$field_value = $order->{"get_$field_key"}();
+
+				if ( empty( $field_value ) ) {
+					throw new RouteException(
+						'headless_checkout_woocommerce_rest_invalid_address',
+						$field_label . '的欄位必填',
+						400
+					);
+				}
+			}
+		}
+	}
+
 }
