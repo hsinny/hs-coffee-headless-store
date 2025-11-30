f52f7c79f1d05030e24b0eaee9c99f619d930db6
hsinny <hsinnyliu@gmail.com>
2025-11-30 18:46:28 +0800

    feat(order): 新增自訂訂單狀態，備貨中(preparing)和已出貨(shipped)

--- hs-coffee-headless-store.php
+++ hs-coffee-headless-store.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HS Coffee Headless Store
  * Description: Headless E-Commerce 客製化解決方案
- * Version: 1.0.0
+ * Version: 1.2.0
  * Author: Hsinny Liu
  */
 
@@ -27,12 +27,14 @@ require_once HS_COFFEE_HEADLESS_STORE_PLUGIN_DIR . 'includes/class-wc-email-cust
 require_once HS_COFFEE_HEADLESS_STORE_PLUGIN_DIR . 'includes/class-wp-frontend-redirect.php';
 require_once HS_COFFEE_HEADLESS_STORE_PLUGIN_DIR . 'includes/class-wc-store-flat-rate-free-shipping.php';
 require_once HS_COFFEE_HEADLESS_STORE_PLUGIN_DIR . 'includes/class-wc-store-ecpay-payment-order.php';
+require_once HS_COFFEE_HEADLESS_STORE_PLUGIN_DIR . 'includes/class-wc-order-custom-statuses.php';
 
 use HS_Coffee_Headless_Store\WC_Store_Checkout;
 use HS_Coffee_Headless_Store\WC_Store_Payment;
 use HS_Coffee_Headless_Store\WC_Email_Custom_Footer;
 use HS_Coffee_Headless_Store\WC_Wp_Frontend_Redirect;
 use HS_Coffee_Headless_Store\WC_Store_Flat_Rate_Free_Shipping;
+use HS_Coffee_Headless_Store\WC_Order_Custom_Statuses;
 
 // 等所有外掛都載入後，再執行我方外掛中的初始化邏輯，避免還沒載入其他依賴外掛就去呼叫會出錯的函式
 add_action( 'plugins_loaded', 'init_headless_checkout_hooks' );
@@ -43,6 +45,7 @@ function init_headless_checkout_hooks() {
 	new WC_Email_Custom_Footer(); // 啟用自訂 Email 佔位符
 	new WC_Wp_Frontend_Redirect(); // 前台訪問控制：只允許管理員訪問 WP 前台
 	new WC_Store_Flat_Rate_Free_Shipping();
+	new WC_Order_Custom_Statuses(); // 註冊自訂訂單狀態
 
 	// 初始化 ECPay 付款訂單類別並註冊路由
 	$ecpay_payment_order = new WC_Store_Ecpay_Payment_Order();
